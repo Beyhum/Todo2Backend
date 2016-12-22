4921f9bc6c07f2839bf8b23fba1c3f358f67b86d
Omar Beyhum <omar.beyhum@gmail.com>
2016-12-22 17:38:48 +0200

    Set base route for controller

--- src/Todo2Backend/Controllers/ItemsController.cs
+++ src/Todo2Backend/Controllers/ItemsController.cs
@@ -7,26 +7,21 @@ using Todo2Backend.Models;
 
 namespace Todo2Backend.Controllers
 {
-    
+    // Instead of setting the entire route path for each action in our controller, we can set a base route path for all actions in a controller by using a route tag above the class name
+    [Route("api/Items")]
     public class ItemsController : Controller
     {
         // GET api/items
-        // This is a Route attribute, which is in charge of mapping a url path to a method in our controller
-        // We call methods in a controller "Actions"
-        [HttpGet("api/items")]
+        [HttpGet] // equivalent to [HttpGet("")]. Since we defined a base route for our controller "api/Items", we call this action through baseurl/api/Items
         public List<Item> Get()
         {
-
             List<Item> itemList = new List<Item> { new Item(1, "Some title", "Valuable notes"), new Item(2, "Get milk", "Make sure it isn't expired...")};
             return itemList;
         }
 
         // GET api/items/5
-        // It's best to follow REST conventions and ommit the Action name when handling resources
-        // Our HTTP Method should specify whether we GET or POST a resource
-        // We can use HttpGet/HttpPost/... instead of Route in our attribute. The difference is that the Http attributes specify which HTTP method should be used
-        [HttpGet("api/items/{id}")]
-        // ASP.NET Core will automatically serialize our objects into JSON and return a response of content-type application/json with a string body representing our object as JSON
+        [HttpGet("{id}")] // Since we defined a base route for our controller "api/Items", we call this action through baseurl/api/Items/id
+        // ASP.NET Core will automatically serialize our objects into JSON and return a response of content-type application/json with a body representing our object
         public Item Get(int id)
         {
             return new Item(id, $"Item of Id = {id}", "Some random message body");
