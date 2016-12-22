b1293e2039b0eebb4debe92dfb80529fae84d1df
Omar Beyhum <omar.beyhum@gmail.com>
2016-12-22 13:06:58 +0200

    Use REST conventions for URL mapping

--- src/Todo2Backend/Controllers/ValuesController.cs
+++ src/Todo2Backend/Controllers/ValuesController.cs
@@ -12,17 +12,17 @@ namespace Todo2Backend.Controllers
         // GET api/values
         // This is a Route attribute, which is in charge of mapping a url path to a method in our controller
         // We call methods in a controller "Actions"
-        // If we go to baseurl/api/values/get then this action will get called, which will simply return an array or strings
-        [Route("api/values/Get")]
+        [HttpGet("api/values")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // MVC will map any url under the form api/values/Get/{id} to this method
-        // it will assign any int that is written after "api/values/Get/" to our id parameter because of the "{id}" route template 
-        // Names specified in {} will be automatically matched to parameters with the same name in the parameter list
-        [Route("api/values/Get/{id}")]
+        // GET api/values/5
+        // It's best to follow REST conventions and ommit the Action name when handling resources
+        // Our HTTP Method should specify whether we GET or POST a resource
+        // We can use HttpGet/HttpPost/... instead of Route in our attribute. The difference is that the Http attributes specify which HTTP method should be used
+        [HttpGet("api/values/{id}")]
         public string Get(int id)
         {
             return "value" + id;
