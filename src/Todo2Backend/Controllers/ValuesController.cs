a814ef4d086fa444f2b5655b730f51291b66d790
Omar Beyhum <omar.beyhum@gmail.com>
2016-12-22 13:02:19 +0200

    Add Get(int id) method, cover route templates

--- src/Todo2Backend/Controllers/ValuesController.cs
+++ src/Todo2Backend/Controllers/ValuesController.cs
@@ -12,12 +12,20 @@ namespace Todo2Backend.Controllers
         // GET api/values
         // This is a Route attribute, which is in charge of mapping a url path to a method in our controller
         // We call methods in a controller "Actions"
-        // So in this case we have the format api/{controller}/{action} which is api/Values/Get
         // If we go to baseurl/api/values/get then this action will get called, which will simply return an array or strings
         [Route("api/values/Get")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+        }
+
+        // MVC will map any url under the form api/values/Get/{id} to this method
+        // it will assign any int that is written after "api/values/Get/" to our id parameter because of the "{id}" route template 
+        // Names specified in {} will be automatically matched to parameters with the same name in the parameter list
+        [Route("api/values/Get/{id}")]
+        public string Get(int id)
+        {
+            return "value" + id;
         }
 
 
