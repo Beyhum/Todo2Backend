a96f7cb54a49cb7506ace97f348f4483280cfca4
Omar Beyhum <omar.beyhum@gmail.com>
2016-12-22 17:23:27 +0200

    Add Item Model, make controller return objects of type Item

--- src/Todo2Backend/Controllers/ValuesController.cs
+++ src/Todo2Backend/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Todo2Backend.Models;
 
 namespace Todo2Backend.Controllers
 {
@@ -13,9 +14,11 @@ namespace Todo2Backend.Controllers
         // This is a Route attribute, which is in charge of mapping a url path to a method in our controller
         // We call methods in a controller "Actions"
         [HttpGet("api/values")]
-        public IEnumerable<string> Get()
+        public List<Item> Get()
         {
-            return new string[] { "value1", "value2" };
+
+            List<Item> itemList = new List<Item> { new Item(1, "Some title", "Valuable notes"), new Item(2, "Get milk", "Make sure it isn't expired...")};
+            return itemList;
         }
 
         // GET api/values/5
@@ -23,9 +26,10 @@ namespace Todo2Backend.Controllers
         // Our HTTP Method should specify whether we GET or POST a resource
         // We can use HttpGet/HttpPost/... instead of Route in our attribute. The difference is that the Http attributes specify which HTTP method should be used
         [HttpGet("api/values/{id}")]
-        public string Get(int id)
+        // ASP.NET Core will automatically serialize our objects into JSON and return a response of content-type application/json with a string body representing our object as JSON
+        public Item Get(int id)
         {
-            return "value" + id;
+            return new Item(id, $"Item of Id = {id}", "Some random message body");
         }
 
 
---
+++ src/Todo2Backend/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo2Backend.Models
+{
+    // our main Model will be the Item class, which represents a single Item in a Todo List
+    public class Item
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Notes { get; set; }
+
+        public Item(int id, string title, string notes)
+        {
+            Id = id;
+            Title = title;
+            Notes = notes;
+        }
+    }
+}
