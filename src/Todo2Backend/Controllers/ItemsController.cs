79012aba47a0de4387f9f91c5d4e6a0cda5c6e25
Omar Beyhum <omar.beyhum@gmail.com>
2016-12-22 17:27:07 +0200

    Rename controller to ItemsController, change route attributes from "values" to "items"

--- src/Todo2Backend/Controllers/ItemsController.cs
+++ src/Todo2Backend/Controllers/ItemsController.cs
@@ -8,12 +8,12 @@ using Todo2Backend.Models;
 namespace Todo2Backend.Controllers
 {
     
-    public class ValuesController : Controller
+    public class ItemsController : Controller
     {
-        // GET api/values
+        // GET api/items
         // This is a Route attribute, which is in charge of mapping a url path to a method in our controller
         // We call methods in a controller "Actions"
-        [HttpGet("api/values")]
+        [HttpGet("api/items")]
         public List<Item> Get()
         {
 
@@ -21,11 +21,11 @@ namespace Todo2Backend.Controllers
             return itemList;
         }
 
-        // GET api/values/5
+        // GET api/items/5
         // It's best to follow REST conventions and ommit the Action name when handling resources
         // Our HTTP Method should specify whether we GET or POST a resource
         // We can use HttpGet/HttpPost/... instead of Route in our attribute. The difference is that the Http attributes specify which HTTP method should be used
-        [HttpGet("api/values/{id}")]
+        [HttpGet("api/items/{id}")]
         // ASP.NET Core will automatically serialize our objects into JSON and return a response of content-type application/json with a string body representing our object as JSON
         public Item Get(int id)
         {
